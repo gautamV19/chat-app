366b0b1c105bf3d34b4bfd4ebe098a4ecc4b7f7a
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-11-25 12:21:30 +0530

    create user

--- src/Resolvers/UserResolver.ts
+++ src/Resolvers/UserResolver.ts
@@ -1,30 +1,7 @@
-import {
-  Resolver,
-  Query,
-  ObjectType,
-  Field,
-  Mutation,
-  Arg,
-  UseMiddleware,
-  Authorized,
-  Ctx,
-} from "type-graphql";
-import User from "../Models/User";
-import { CreateUserInput } from "../helper/input";
-import jwt from "jsonwebtoken";
-import MyContext from "../helper/context";
-import bcrypt from "bcryptjs";
+import User from "src/Models/User";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
-@ObjectType()
-class LoginOutput {
-  @Field(() => User)
-  user: User;
-
-  @Field()
-  token: string;
-}
-
-@Resolver(() => String)
+@Resolver()
 class UserResolver {
   @Query(() => String)
   greet() {
@@ -32,53 +9,10 @@ class UserResolver {
   }
 
   @Mutation(() => Boolean)
-  @UseMiddleware()
-  async createUser(@Arg("data") createUserInput: CreateUserInput) {
-    const user = User.create({
-      name: createUserInput.name,
-      email: createUserInput.email,
-      password: createUserInput.password,
-    });
+  createUser(@Arg("username") name: string) {
+    const user = User.create({ name });
     user.save();
-
     return !!user;
-  }
-
-  @Mutation(() => LoginOutput)
-  async login(@Arg("email") email: string, @Arg("password") password: string) {
-    const user: any = await User.findOne({ email: email });
-    // why I can't check if user exist or not
-
-    const isMatched = await bcrypt.compare(password, user.password);
-    let token = "";
-    if (isMatched) {
-      token = jwt.sign({ email }, "jay swaminarayan");
-    }
-    return { user, token };
-  }
-
-  @Query(() => [User])
-  @Authorized()
-  getUser() {
-    const users = User.find();
-    return users;
-  }
-
-  // //** Not needed for now */
-  // @Query(() => Task)
-  // getTask() {
-  //   const task = new Task();
-  //   task.title = "jsn";
-  //   task.description = "hihi";
-  //   task.time = "7.13pm";
-
-  //   return task;
-  // }
-
-  @Query(() => User)
-  @Authorized()
-  me(@Ctx() { user }: MyContext) {
-    return user;
   }
 }
 
